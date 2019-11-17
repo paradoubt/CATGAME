63ffee8b62aa457fac212ada191d363f4dcc5c45
paradoubt <mariana.lobeto@knights.ucf.edu>
2019-11-17 08:24:12 -0500

    game

---
+++ Tutorial2Tilemap/Assets/Scripts/CatManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatManager : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+    Animator anim;
+    private bool facingRight = true;
+    public float speed;
+  
+
+
+
+
+
+
+    // Start is called before the first frame update
+    //I WILL GET THIS CAT RUNNING
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        {
+            rb2d = GetComponent<Rigidbody2D>();
+        }
+
+        // Walking to Idle
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            anim.SetInteger("State", 1);
+        }
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            anim.SetInteger("State", 0);
+        }
+        // for first animation
+
+        // Jumping to Idle
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            anim.SetInteger("State", 2);
+        }
+        if (Input.GetKeyUp(KeyCode.R))
+        {
+            anim.SetInteger("State", 0);
+        }
+        // for second animation
+    }
+    void FixedUpdate()
+    {
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        //Vector2 movement = new Vector2(moveHorizontal, 0);
+
+        // rb2d.AddForce(movement * speed);
+        
+
+    }
+}
---
+++ Tutorial2Tilemap/Assets/Scripts/Test.cs
@@ -0,0 +1,138 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Rigidbody2D rb2d;
+    private bool facingRight = true;
+    private int count;
+
+    public float speed;
+    public float jumpforce;
+    public int numberOfCoins;
+    public Text countText;
+
+    //ground check
+    private bool isOnGround;
+    public Transform groundcheck;
+    public float checkRadius;
+    public LayerMask allGround;
+
+    // private float jumpTimeCounter;
+    //public float jumpTime;
+    //private bool isJumping;
+
+    //audio stuff
+
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        count = 0;
+        SetCountText();
+    }
+
+    void Awake()
+    {
+
+        // source = GetComponent<AudioSource>();
+
+    }
+
+    private void Update()
+    {
+
+    }
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        //Vector2 movement = new Vector2(moveHorizontal, 0);
+
+        // rb2d.AddForce(movement * speed);
+
+        rb2d.velocity = new Vector2(moveHorizontal * speed, rb2d.velocity.y);
+
+        isOnGround = Physics2D.OverlapCircle(groundcheck.position, checkRadius, allGround);
+
+        Debug.Log(isOnGround);
+
+
+
+        //stuff I added to flip my character
+        if (facingRight == false && moveHorizontal > 0)
+        {
+            Flip();
+        }
+        else if (facingRight == true && moveHorizontal < 0)
+        {
+            Flip();
+        }
+
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector2 Scaler = transform.localScale;
+        Scaler.x = Scaler.x * -1;
+        transform.localScale = Scaler;
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Ground" && isOnGround)
+        {
+
+
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                // rb2d.AddForce(new Vector2(0, jumpforce), ForceMode2D.Impulse);
+                rb2d.velocity = Vector2.up * jumpforce;
+
+
+                // Audio stuff
+
+
+
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        //Check the provided Collider2D parameter other to see if it is tagged "PickUp", if it is...
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+
+    /*private void OnColisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "CoinBox" && playerHit == true)
+        {
+            count = count + 1;
+            SetCountText();
+            numberOfCoins = numberOfCoins - 1;
+        }
+    }
+    */
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count;
+    }
+}
